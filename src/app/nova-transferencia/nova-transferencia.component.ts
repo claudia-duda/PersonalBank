b1e148fae382a0165225c902e9afe868668d449f
Claudia Ramos <claudiaeduarda_duda@hotmail.com>
2022-09-12 11:21:55 -0300

    Using data binding

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
+export class NovaTransferenciaComponent {
 
-export class NovaTransferenciaComponent{
+  valor: number = 0;
+  destino: number = 0;
 
+  transferir() {
+    console.log('Solicitada nova transfência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+
+  }
 }
