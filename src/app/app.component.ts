aece56d58eeb2b20ba4426fc2570a866ccbb3cda
Vinicius Claudino <gnu.vinicius@gmail.com>
2022-09-14 09:53:22 -0300

    validando tranferencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +8,6 @@ import { TransferenciaService } from './services/transferencia.service';
 export class AppComponent {
   title = 'bytebank';
 
-  constructor(private service: TransferenciaService) {}
+  constructor() {}
 
 }
